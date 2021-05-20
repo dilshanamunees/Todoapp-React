6786999f6cde2d780ebe13690cc3d6ea8ec607f2
dilshanamunees <dilshanaibrahim@gmail.com>
2021-05-20 01:08:40 -0700

    final commit

--- todolist/src/App.js
+++ todolist/src/App.js
@@ -3,8 +3,13 @@ import './App.css';
 import {useState} from "react"
 
 function App() {
+  const days = [ "Sunday", "Monday","Tuesday", "Wednesday","Thursday", "Friday","Saturday"]
+  
   const [toDos,setToDos]=useState([]) //list of todos as arrays in state
   const [toDo,setTodo]=useState("")//input field state for validation
+  const [cancelled,setCancelled]=useState([])
+ 
+
   return (
     <div className="app">
     <div className="mainHeading">
@@ -12,17 +17,17 @@ function App() {
     </div>
     <div className="subHeading">
       <br />
-      <h2>Whoop, it's Wednesday 🌝 ☕ </h2>
+      <h2>Whoop, it's {days[new Date().getDay()]} 🌝 ☕ </h2>
     </div>
     <div className="input">
       <input value={toDo} onChange={(e)=>{setTodo(e.target.value)}} type="text" placeholder="🖊️ Add item..." />
-      <i onClick={()=>{setToDos([...toDos,{id:Date.now(),text:toDo,status:false}])}} className="fas fa-plus"></i>
+      <i onClick={()=>{setToDos([...toDos,{id:Date.now(),text:toDo,status:false,deleted:false}])}} className="fas fa-plus"></i>
     </div>
     <div className="todos">
       {toDos.map((obj)=>{
-
+          if(obj.deleted!==true){
       return(
-        //remove checked todos from list
+        
       <div className="todo">
         <div className="left">
          
@@ -40,20 +45,64 @@ function App() {
           <p>{obj.text}</p>
         </div>
         <div className="right">
-          <i  className="fas fa-times"></i>
+          <i onClick={()=>{
+            
+            setToDos(
+              toDos.filter((toDoItem) => {
+                if (toDoItem.id === obj.id) {
+                  toDoItem.deleted = true;
+                  !toDoItem.status &&
+                    setCancelled([...cancelled, toDoItem.text]);
+                }
+                return toDoItem;
+              })
+            );
+            
+          }} className="fas fa-times"></i>
         </div>
       </div>)
-      })}
+ }
+ return null
+  })}
+      <div className="history">
+       <h3>Active task</h3>
       {
         toDos.map((obj)=>{
-          if(obj.status){
+          if(obj.status !==true && obj.deleted !==true){
             return(
-              <h1>{obj.text}</h1>
+              <p key={obj.id}>{obj.text}</p>
             )
           }
           return (null)
-        })
+  })
       }
+     </div>
+     <div className="history">
+       <h3>completed task</h3>
+       {
+         toDos.map((obj=>{
+           if(obj.status===true){
+           return (
+             <p >{obj.text}</p>
+
+           )}
+           return null
+         }))
+       }
+
+     </div>
+     <div className="history">
+       <h3>cancelled task</h3>
+       {
+         cancelled.map((obj)=>{
+           return(
+             <p key={obj.id}>{obj}</p>
+           )
+
+         })
+       }
+
+     </div>
     </div>
   </div>
    );
